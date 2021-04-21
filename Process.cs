79d614e2881cf2b418b0ca0fd379d17c15ba8901
nermeen_atif <1701571@eng.asu.edu.eg>
2021-04-22 01:22:23 +0200

    add main and process functions

---
+++ Process.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Process
+    {
+
+        private int Process_ID; // Process ID
+        private int Burst_Time; // Burst Time
+        private int Arrival_Time; // Arrival Time
+
+
+
+
+        public Process(int Process_ID, int Burst_Time, int Arrival_Time)
+        {
+            this.Process_ID = Process_ID;
+            this.Burst_Time = Burst_Time;
+            this.Arrival_Time = Arrival_Time;
+        }
+        public void set_Process_ID(int Process_ID)
+        {
+            this.Process_ID = Process_ID;
+        }
+        public int get_Process_ID()
+        {
+            return this.Process_ID;
+        }
+        public void set_Burst_Time(int Burst_Time)
+        {
+            this.Burst_Time = Burst_Time;
+        }
+        public int get_Burst_Time()
+        {
+            return this.Burst_Time;
+        }
+        public void set_Arrival_Time(int Arrival_Time)
+        {
+            this.Arrival_Time = Arrival_Time;
+        }
+        public int get_Arrival_Time()
+        {
+            return this.Arrival_Time;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ConsoleApp1
+{
+
+   
+    class Program
+    {
+        
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Enter number of Process: ");
+            int num_procees = Convert.ToInt32(Console.ReadLine());
+            
+            Process[] mat_process = new Process[num_procees] ;
+           
+            for (int i = 0; i < mat_process.Length; i++)
+            {
+                mat_process[i] = new Process(0,0,0);
+
+            }
+
+            
+            
+            for (int i = 0; i < num_procees; i++)
+            {
+
+                Console.WriteLine("...Process ", i + 1, "...\n");
+                Console.WriteLine("Enter Process Id: ");
+                mat_process[i].set_Process_ID(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine("Enter Arrival Time: ");
+                mat_process[i].set_Arrival_Time(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine("Enter Burst Time: ");
+                mat_process[i].set_Burst_Time(Convert.ToInt32(Console.ReadLine()));
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
