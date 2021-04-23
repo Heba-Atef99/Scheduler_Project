b1eb1c3bf538c1dfebac937980f64ec4b32d3237
noura <1701595@eng.asu.edu.eg>
2021-04-24 01:45:14 +0200

    non pree

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ namespace ConsoleApp1
             for (int i = 0; i < num_process; i++)
             {
                 //initialize the process
-                mat_process[i] = new Process(0, 0, 0,0,0);
+                mat_process[i] = new Process(0, 0, 0, 0,0);
 
                 Console.WriteLine("\n...Process {0}...", i + 1);
 
@@ -34,35 +34,82 @@ namespace ConsoleApp1
                 Console.WriteLine("Enter Burst Time: ");
                 mat_process[i].set_Burst_Time(Convert.ToInt32(Console.ReadLine()));
 
-               
+
                 Console.WriteLine("Priority: ");
                 mat_process[i].set_Priority(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine("Waiting Time: ");
+                mat_process[i].set_Priority(Convert.ToInt32(Console.ReadLine()));
             }
-            SFJ_Preemptive(mat_process, scheduler_history);
 
+            /* Process[] mat_process = new Process[4];
+
+
+             mat_process[0] = new Process(1, 0, 6, 2, 0);
+             mat_process[1] = new Process(2, 5, 8, 1, 0);
+             mat_process[2] = new Process(3, 0, 7, 3, 0);
+             mat_process[3] = new Process(4, 1, 3, 4, 0);
+             int[,] scheduler_history = new int[4, 3];
+             Priority_non_Preemptive(mat_process, scheduler_history);
+             for (int i = 0; i < 4; i++)
+             {
+                 //Console.WriteLine(mat_process[i].get_Waiting_Time());
+                 Console.WriteLine(" " + scheduler_history[i, 0] + "\t\t" + scheduler_history[i, 1] + "\t\t " + scheduler_history[i, 2]);
+
+             }*/
+            Priority_non_Preemptive(mat_process,  scheduler_history);
         }
 
-        static void SFJ_Preemptive(Process[] mat_process, int[,] scheduler_history)
+        static void Priority_non_Preemptive(Process[] mat_process, int[,] scheduler_history)
         {
-            //test the program
-            int id, at, bt;
-            for (int i = 0; i < mat_process.Length; i++)
+            int number = 0;
+            Process Current_process = new Process(0, 0, 0, 0, 0);
+            Current_process = mat_process[0];
+            int End_time = 0;
+            int Count = 0;
+            for (int j = Count; j < mat_process.Length; j++)
             {
-                Console.WriteLine("\n...Process {0}...", i + 1);
+                for (int i = Count; i < mat_process.Length; i++)
+                {
+                    if (mat_process[i].get_Arrival_Time() <= End_time)
+                    {
+                        Current_process = mat_process[i];
+                        number = i;
+                        break;
+                    }
 
-                id = mat_process[i].get_Process_ID();
-                Console.WriteLine("Process Id: {0}", id);
+                }
+                for (int i = Count; i < mat_process.Length; i++)
+                {
+                    if (mat_process[i].get_Arrival_Time() <= End_time && mat_process[i].get_Priority() < Current_process.get_Priority())
+                    {
+                        Current_process = mat_process[i];
+                        number = i;
+                    }
 
-                at = mat_process[i].get_Arrival_Time();
-                Console.WriteLine("Enter Arrival Time: {0}", at);
+                }
+                scheduler_history[Count, 0] = Current_process.get_Process_ID();
+                scheduler_history[Count, 1] = End_time;
+                mat_process[number].set_Waiting_Time(End_time - mat_process[number].get_Arrival_Time());
+                End_time = End_time + Current_process.get_Burst_Time();
+                scheduler_history[Count, 2] = End_time;
+                Swap(ref mat_process[number], ref mat_process[Count]);
+                Count++;
 
-                bt = mat_process[i].get_Burst_Time();
-                Console.WriteLine("Enter Burst Time: {0}", bt);
             }
+            for (int i = 0; i <scheduler_history.Length ; i++)
+            {
+                
+                Console.WriteLine(" " + scheduler_history[i, 0] + "\t\t" + scheduler_history[i, 1] + "\t\t " + scheduler_history[i, 2]);
 
-            //Console.ReadLine();
-
+            }
+        }
+        //swap function
+        public static void Swap(ref Process num1, ref Process num2)
+        {
+            Process newnum = new Process(0, 0, 0, 0, 0);
+            newnum = num1;
+            num1 = num2;
+            num2 = newnum;
         }
     }
 }
-
