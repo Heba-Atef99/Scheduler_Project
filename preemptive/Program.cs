44921400c103d231ee3255ff6b9457b0428a18fc
Heba-Atef99 <hoba30.ha@gmail.com>
2021-04-24 21:49:24 +0200

    almost working, needs waiting time & more tests

--- preemptive/Program.cs
+++ preemptive/Program.cs
@@ -15,9 +15,9 @@ namespace ConsoleApp1
 
             //the history of processes as the element of this array consists of 3 parts
             //first the process id, second the start time of the process, third the end time of the process
-            int[,] scheduler_history = new int[(num_process + 1), 3];
+            int[,] scheduler_history = new int[num_process, 3];
 
-            List<sh_element> scheduler_history = new List<sh_element>();
+            List<sh_element> scheduler_history_list = new List<sh_element>();
 
 
             //take input from user & save it in objects of processes
@@ -32,20 +32,20 @@ namespace ConsoleApp1
                 mat_process[i].set_Process_ID(Convert.ToInt32(Console.ReadLine()));
 
                 Console.WriteLine("Enter Arrival Time: ");
-                mat_process[i].set_Arrival_Time(Convert.ToInt32(Console.ReadLine()));
+                mat_process[i].set_Arrival_Time(float.Parse(Console.ReadLine()));
 
                 Console.WriteLine("Enter Burst Time: ");
-                mat_process[i].set_Burst_Time(Convert.ToInt32(Console.ReadLine()));
+                mat_process[i].set_Burst_Time(float.Parse(Console.ReadLine()));
 
 
-                Console.WriteLine("Priority: ");
-                mat_process[i].set_Priority(Convert.ToInt32(Console.ReadLine()));
+                //Console.WriteLine("Priority: ");
+                //mat_process[i].set_Priority(Convert.ToInt32(Console.ReadLine()));
             }
-            SJF_Preemptive(mat_process, scheduler_history);
+            SJF_Preemptive(mat_process, scheduler_history_list);
 
         }
 
-        static void SJF_Preemptive(Process[] mat_process, int[,] scheduler_history)
+        static void SJF_Preemptive(Process[] mat_process, List<sh_element> scheduler_history)
         {
             //save the array in a list to make operations on list easily
             List<Process> ready = mat_process.ToList();
@@ -55,10 +55,10 @@ namespace ConsoleApp1
             //waiting list for waiting processes
             List<Process> wait = new List<Process>();
 
-            int next_arrival = ready[1].get_Arrival_Time(); //arrival of the next process
-            int current_arrival = ready[0].get_Arrival_Time(); //arrival of the next process
-            int current_burst_time_left = 0; //execution time left for the process
-            int current_end = 0;
+            float next_arrival = ready[1].get_Arrival_Time(); //arrival of the next process
+            float current_arrival = ready[0].get_Arrival_Time(); //arrival of the next process
+            float current_burst_time_left = 0; //execution time left for the process
+            float current_end = 0;
             int sh_index = 0; //index of the scheduler history array
 
             //start a process
@@ -67,9 +67,14 @@ namespace ConsoleApp1
                 if(ready[0].get_Burst_Time() < ready[1].get_Burst_Time())
                 {
                     //start the current process
-                    scheduler_history[sh_index, 0] = ready[0].get_Process_ID();
-                    scheduler_history[sh_index, 1] = current_arrival;
-                    scheduler_history[sh_index++, 2] = current_arrival + ready[0].get_Burst_Time();
+                    scheduler_history.Add(new sh_element()
+                    {
+                        process_id = ready[0].get_Process_ID(),
+                        start = current_arrival,
+                        end = current_arrival + ready[0].get_Burst_Time()
+                    });
+                    sh_index++;
+
                     //put next process in wait & remove from ready
                     wait.Add(ready[1]);
                     ready.RemoveAt(1);
@@ -78,9 +83,13 @@ namespace ConsoleApp1
                 else
                 {
                     //start the current process
-                    scheduler_history[sh_index, 0] = ready[1].get_Process_ID();
-                    scheduler_history[sh_index, 1] = current_arrival;
-                    scheduler_history[sh_index++, 2] = current_arrival + ready[1].get_Burst_Time();
+                    scheduler_history.Add(new sh_element()
+                    {
+                        process_id = ready[1].get_Process_ID(),
+                        start = current_arrival,
+                        end = current_arrival + ready[1].get_Burst_Time()
+                    });
+                    sh_index++;
 
                     //put next process in wait & remove from ready
                     wait.Add(ready[0]);
@@ -91,10 +100,13 @@ namespace ConsoleApp1
             else
             {
                 //start the current process
-                scheduler_history[sh_index, 0] = ready[0].get_Process_ID();
-                scheduler_history[sh_index, 1] = current_arrival;
-                scheduler_history[sh_index++, 2] = current_arrival + ready[0].get_Burst_Time();
-
+                scheduler_history.Add(new sh_element()
+                {
+                    process_id = ready[0].get_Process_ID(),
+                    start = current_arrival,
+                    end = current_arrival + ready[0].get_Burst_Time()
+                });
+                sh_index++;
             }
 
             //for each process starting from process 2 
@@ -102,7 +114,7 @@ namespace ConsoleApp1
             {
                 next_arrival = ready[1].get_Arrival_Time();
                 current_arrival = ready[0].get_Arrival_Time();
-                current_end = scheduler_history[sh_index - 1, 2];
+                current_end = scheduler_history[sh_index - 1].end;
 
                 if (next_arrival < current_end)
                 {
@@ -117,15 +129,18 @@ namespace ConsoleApp1
                     {
                         //inturrept current process
                         //end current process
-                        scheduler_history[sh_index - 1, 2] = next_arrival;
+                        scheduler_history[sh_index - 1].end = next_arrival;
                         wait.Add(ready[0]);
                         ready.RemoveAt(0);
 
                         //start next process
-                        scheduler_history[sh_index, 0] = ready[0].get_Process_ID();
-                        scheduler_history[sh_index, 1] = next_arrival;
-                        scheduler_history[sh_index++, 2] = next_arrival + ready[0].get_Burst_Time();
-
+                        scheduler_history.Add(new sh_element()
+                        {
+                            process_id = ready[0].get_Process_ID(),
+                            start = next_arrival,
+                            end = next_arrival + ready[0].get_Burst_Time()
+                        });
+                        sh_index++;
                     }
                     else
                     {
@@ -137,20 +152,37 @@ namespace ConsoleApp1
                 else if (next_arrival > current_end)
                 {
                     //end current process
-                    scheduler_history[sh_index - 1, 2] = current_arrival + ready[0].get_Burst_Time();
+                    //scheduler_history[sh_index - 1].end = current_arrival + ready[0].get_Burst_Time();
                     ready.RemoveAt(0);
 
                     if (wait.Count > 0)
                     {
                         wait = wait.OrderBy(process => process.get_Burst_Time()).ToList();
                         //start waiting process
-                        scheduler_history[sh_index, 0] = wait[0].get_Process_ID();
-                        scheduler_history[sh_index++, 1] = scheduler_history[sh_index - 2, 2];
+                        scheduler_history.Add(new sh_element()
+                        {
+                            process_id = wait[0].get_Process_ID(),
+                            start = scheduler_history[sh_index - 1].end,
+                            end = scheduler_history[sh_index - 1].end + wait[0].get_Burst_Time()
+                        });
+                        sh_index++;
+
                         ready.Insert(0, wait[0]);
                         wait.RemoveAt(0);
                         //update burst time
-                        current_burst_time_left = ready[0].get_Burst_Time() - (ready[1].get_Arrival_Time() - scheduler_history[sh_index - 1, 1]);
+                        current_burst_time_left = ready[0].get_Burst_Time() - (ready[1].get_Arrival_Time() - scheduler_history[sh_index - 1].start);
                         ready[0].set_Burst_Time(current_burst_time_left);
+                    }
+                    else
+                    {
+                        //start next process
+                        scheduler_history.Add(new sh_element()
+                        {
+                            process_id = ready[0].get_Process_ID(),
+                            start = next_arrival,
+                            end = next_arrival + ready[0].get_Burst_Time()
+                        });
+                        sh_index++;
 
                     }
                 }
@@ -160,21 +192,32 @@ namespace ConsoleApp1
                     //next process starts right after the current process
 
                     //end current process
-                    scheduler_history[sh_index - 1, 2] = current_arrival + ready[0].get_Burst_Time();
+                    //scheduler_history[sh_index - 1].end = current_arrival + ready[0].get_Burst_Time();
+                    scheduler_history[sh_index - 1].end = next_arrival;
                     ready.RemoveAt(0);
 
                     wait = wait.OrderBy(process => process.get_Burst_Time()).ToList();
                     if (ready[0].get_Burst_Time() < wait[0].get_Burst_Time())
                     {
                         //start next process
-                        scheduler_history[sh_index, 0] = ready[0].get_Process_ID();
-                        scheduler_history[sh_index++, 1] = next_arrival;
+                        scheduler_history.Add(new sh_element()
+                        {
+                            process_id = ready[0].get_Process_ID(),
+                            start = next_arrival,
+                            end = next_arrival + ready[0].get_Burst_Time()
+                        });
+                        sh_index++;
                     }
                     else
                     {
                         //start waiting process
-                        scheduler_history[sh_index, 0] = wait[0].get_Process_ID();
-                        scheduler_history[sh_index++, 1] = scheduler_history[sh_index - 2, 2];
+                        scheduler_history.Add(new sh_element()
+                        {
+                            process_id = wait[0].get_Process_ID(),
+                            start = scheduler_history[sh_index - 1].end,
+                            end = scheduler_history[sh_index - 1].end + wait[0].get_Burst_Time()
+                        });
+                        sh_index++;
 
                         //put next process in wait
                         wait.Add(ready[0]);
@@ -192,17 +235,21 @@ namespace ConsoleApp1
             for (int i = 0; i < wait.Count; i++)
             {
                 //save the start & end time of the shortest remaining time process
-                scheduler_history[sh_index, 0] = wait[i].get_Process_ID();
-                scheduler_history[sh_index, 1] = scheduler_history[sh_index - 1, 2]; //start of the current process is the end of prev process
-                scheduler_history[sh_index, 2] = wait[i].get_Burst_Time() + scheduler_history[sh_index, 1]; //end of current process is the start of this process + burst time left
+                scheduler_history.Add(new sh_element()
+                {
+                    process_id = wait[i].get_Process_ID(),
+                    start = scheduler_history[sh_index - 1].end,
+                    end = wait[i].get_Burst_Time() + scheduler_history[sh_index - 1].end
+                });
+
                 sh_index++;
             }
 
-            for (int i = 0; i < scheduler_history.Length; i++)
+            for (int i = 0; i < scheduler_history.Count; i++)
             {
-                Console.WriteLine("Process Id: {0}\n", scheduler_history[i, 0]);
-                Console.WriteLine("Start Time: {0}\n", scheduler_history[i, 1]);
-                Console.WriteLine("End Time: {0}\n", scheduler_history[i, 2]);
+                Console.WriteLine("Process Id: {0}", scheduler_history[i].process_id);
+                Console.WriteLine("Start Time: {0}", scheduler_history[i].start);
+                Console.WriteLine("End Time: {0}", scheduler_history[i].end);
                 Console.WriteLine("\n\n");
             }
 
