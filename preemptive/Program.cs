a1cf92a2ca45f4f045f4faff9dafa11537dc0fbe
Heba-Atef99 <hoba30.ha@gmail.com>
2021-04-25 15:34:58 +0200

    all good but waiting time

--- preemptive/Program.cs
+++ preemptive/Program.cs
@@ -50,7 +50,7 @@ namespace ConsoleApp1
             //save the array in a list to make operations on list easily
             List<Process> ready = mat_process.ToList();
             //sort the ready processes by the arrival time
-            ready = ready.OrderBy(process => process.get_Arrival_Time()).ToList();
+            ready = ready.OrderBy(process => process.get_Arrival_Time()).ThenBy(process => process.get_Burst_Time()).ToList();
 
             //waiting list for waiting processes
             List<Process> wait = new List<Process>();
@@ -61,7 +61,7 @@ namespace ConsoleApp1
             float current_end = 0;
             int sh_index = 0; //index of the scheduler history array
 
-            //start a process
+            //execute the first process
             if (next_arrival == current_arrival)
             {
                 if(ready[0].get_Burst_Time() < ready[1].get_Burst_Time())
@@ -109,8 +109,8 @@ namespace ConsoleApp1
                 sh_index++;
             }
 
-            //for each process starting from process 2 
-            for (int i = 1; ready.Count != 1; i++)
+            //executing each process in ready list starting from process 2 
+            while (ready.Count != 1)
             {
                 next_arrival = ready[1].get_Arrival_Time();
                 current_arrival = ready[0].get_Arrival_Time();
@@ -121,13 +121,12 @@ namespace ConsoleApp1
                     //an intrrupt happens
 
                     //update the burst time
-                    //current_burst_time_left = ready[0].get_Burst_Time() - (next_arrival - current_arrival);
                     current_burst_time_left = current_end - next_arrival;
                     ready[0].set_Burst_Time(current_burst_time_left);
 
                     if (ready[1].get_Burst_Time() < current_burst_time_left)
                     {
-                        //inturrept current process
+                        //inturrept current process &
                         //end current process
                         scheduler_history[sh_index - 1].end = next_arrival;
                         wait.Add(ready[0]);
@@ -144,6 +143,8 @@ namespace ConsoleApp1
                     }
                     else
                     {
+                        //continue the current process
+                        //put next process in wait & remove it from ready list
                         wait.Add(ready[1]);
                         ready.RemoveAt(1);
                     }
@@ -151,14 +152,15 @@ namespace ConsoleApp1
 
                 else if (next_arrival > current_end)
                 {
-                    //end current process
-                    //scheduler_history[sh_index - 1].end = current_arrival + ready[0].get_Burst_Time();
+                    //the current process reached its end
                     ready.RemoveAt(0);
 
+                    //check the waiting list 
                     if (wait.Count > 0)
                     {
-                        wait = wait.OrderBy(process => process.get_Burst_Time()).ToList();
-                        //start waiting process
+                        wait = wait.OrderBy(process => process.get_Burst_Time()).ThenBy(process => process.get_Arrival_Time()).ToList();
+
+                        //start waiting process as the next process has not arrived yet
                         scheduler_history.Add(new sh_element()
                         {
                             process_id = wait[0].get_Process_ID(),
@@ -166,10 +168,11 @@ namespace ConsoleApp1
                             end = scheduler_history[sh_index - 1].end + wait[0].get_Burst_Time()
                         });
                         sh_index++;
-
+                        //insert that waiting process in ready & remove it from wait list
                         ready.Insert(0, wait[0]);
                         wait.RemoveAt(0);
-                        //update burst time
+
+                        //update burst time as it is used in waiting list for ordering it 
                         current_burst_time_left = ready[0].get_Burst_Time() - (ready[1].get_Arrival_Time() - scheduler_history[sh_index - 1].start);
                         ready[0].set_Burst_Time(current_burst_time_left);
                     }
@@ -183,55 +186,58 @@ namespace ConsoleApp1
                             end = next_arrival + ready[0].get_Burst_Time()
                         });
                         sh_index++;
-
                     }
                 }
 
                 else
                 {
-                    //next process starts right after the current process
+                    //next process starts right after the current process finishes
 
                     //end current process
-                    //scheduler_history[sh_index - 1].end = current_arrival + ready[0].get_Burst_Time();
                     scheduler_history[sh_index - 1].end = next_arrival;
                     ready.RemoveAt(0);
 
-                    wait = wait.OrderBy(process => process.get_Burst_Time()).ToList();
-                    if (ready[0].get_Burst_Time() < wait[0].get_Burst_Time())
+                    if (wait.Count > 0)
                     {
-                        //start next process
-                        scheduler_history.Add(new sh_element()
+                        //compare the next process with the waiting process
+                        wait = wait.OrderBy(process => process.get_Burst_Time()).ThenBy(process => process.get_Arrival_Time()).ToList();
+                        if (ready[0].get_Burst_Time() > wait[0].get_Burst_Time())
                         {
-                            process_id = ready[0].get_Process_ID(),
-                            start = next_arrival,
-                            end = next_arrival + ready[0].get_Burst_Time()
-                        });
-                        sh_index++;
+                            //start waiting process
+                            scheduler_history.Add(new sh_element()
+                            {
+                                process_id = wait[0].get_Process_ID(),
+                                start = scheduler_history[sh_index - 1].end,
+                                end = scheduler_history[sh_index - 1].end + wait[0].get_Burst_Time()
+                            });
+                            sh_index++;
+
+                            //put next process in wait
+                            wait.Add(ready[0]);
+                            ready.RemoveAt(0);
+
+                            //insert the waited process in ready
+                            ready.Insert(0, wait[0]);
+                            wait.RemoveAt(0);
+
+                            //skip the loop so that the code this condition wont get executed
+                            continue;
+                        }
                     }
-                    else
+
+                    //start next process
+                    scheduler_history.Add(new sh_element()
                     {
-                        //start waiting process
-                        scheduler_history.Add(new sh_element()
-                        {
-                            process_id = wait[0].get_Process_ID(),
-                            start = scheduler_history[sh_index - 1].end,
-                            end = scheduler_history[sh_index - 1].end + wait[0].get_Burst_Time()
-                        });
-                        sh_index++;
-
-                        //put next process in wait
-                        wait.Add(ready[0]);
-                        ready.RemoveAt(0);
-
-                        //insert the waited process in ready
-                        ready.Insert(0, wait[0]);
-                        wait.RemoveAt(0);
-                    }
+                        process_id = ready[0].get_Process_ID(),
+                        start = next_arrival,
+                        end = next_arrival + ready[0].get_Burst_Time()
+                    });
+                    sh_index++;
                 }
             }
 
-            wait = wait.OrderBy(process => process.get_Burst_Time()).ToList();
-            //check all processes in the waiting list
+            wait = wait.OrderBy(process => process.get_Burst_Time()).ThenBy(process => process.get_Arrival_Time()).ToList();
+            //execute all processes in the waiting list
             for (int i = 0; i < wait.Count; i++)
             {
                 //save the start & end time of the shortest remaining time process
@@ -241,20 +247,112 @@ namespace ConsoleApp1
                     start = scheduler_history[sh_index - 1].end,
                     end = wait[i].get_Burst_Time() + scheduler_history[sh_index - 1].end
                 });
-
                 sh_index++;
+            }
+
+            //calculate the waiting time for each process
+            List<sh_element> get_waiting_time = scheduler_history.OrderBy(process => process.process_id).ThenBy(process => process.start).ToList();
+            int id = get_waiting_time[0].process_id;
+            float waiting_time = 0;
+            int is_last_repeated = 0;//to check if the last element in list is repeated or not
+            float first_start = get_waiting_time[0].start;
+            int j = 1;
+
+            while (get_waiting_time.Count != 1)
+            {
+                j = 1;
+                is_last_repeated = 0;
+                //calculate the waiting time for un-repeated process
+                //waiting time = start of current process - arrival time for that process
+                waiting_time = get_waiting_time[0].start - Array.Find(mat_process, p => p.get_Process_ID() == id).get_Arrival_Time();
+
+                while (get_waiting_time[j].process_id == id)
+                {
+                    //calculate the waiting time for repeated process
+                    //waiting time = (start of next same process - end of last same process) + prev value
+                    waiting_time += (get_waiting_time[j].start - get_waiting_time[j - 1].end);
+                    j++;
+                    is_last_repeated = 1;
+                }
+                //save it in the process
+                Array.Find(mat_process, p => p.get_Process_ID() == id).set_Waiting_Time(waiting_time);
+
+                //remove all elements with same id
+                get_waiting_time.RemoveRange(0, j - 1);
+                //update id
+                id = get_waiting_time[0].process_id;
+            }
+
+            //for the last element in the list
+            if (is_last_repeated != 1)
+            {
+                //calculate the waiting time of the last element if it is not repeated
+                waiting_time = get_waiting_time[0].start - Array.Find(mat_process, p => p.get_Process_ID() == id).get_Arrival_Time();
+
+                //save that waiting time
+                Array.Find(mat_process, p => p.get_Process_ID() == id).set_Waiting_Time(waiting_time);
             }
 
             for (int i = 0; i < scheduler_history.Count; i++)
             {
+                Console.WriteLine("\n");
                 Console.WriteLine("Process Id: {0}", scheduler_history[i].process_id);
                 Console.WriteLine("Start Time: {0}", scheduler_history[i].start);
                 Console.WriteLine("End Time: {0}", scheduler_history[i].end);
-                Console.WriteLine("\n\n");
+                Console.WriteLine("\n");
             }
+
+            for (int i = 0; i < mat_process.Length; i++)
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("Process Id: {0} ,, waiting time {1}", mat_process[i].get_Process_ID(), mat_process[i].get_Waiting_Time());
+                Console.WriteLine("\n");
+            }
+
 
 
         }
     }
 }
 
+/*
+while (get_waiting_time.Count != 1)
+{
+    if (get_waiting_time[1].process_id == id)
+    {
+        //calculate the waiting time for repeated process
+        //waiting time = (start of next same process - end of last same process) + (start of first time for process - arrival)
+        waiting_time += ((get_waiting_time[1].start - get_waiting_time[0].end) + (first_start - Array.Find(mat_process, p => p.get_Process_ID() == id).get_Arrival_Time()));
+
+        //save that waiting time
+        Array.Find(mat_process, p => p.get_Process_ID() == id).set_Waiting_Time(waiting_time);
+
+        is_last_repeated = 1;
+    }
+    else
+    {
+        //check if the first element is repeated or not
+        if (is_last_repeated == 1)
+        {
+            get_waiting_time.RemoveAt(0);
+            //update id
+            id = get_waiting_time[0].process_id;
+        }
+
+        //calculate the waiting time for un-repeated process
+        //waiting time = start of current process - arrival time for that process
+        waiting_time = get_waiting_time[0].start - Array.Find(mat_process, p => p.get_Process_ID() == id).get_Arrival_Time();
+
+        //save that waiting time
+        Array.Find(mat_process, p => p.get_Process_ID() == id).set_Waiting_Time(waiting_time);
+
+        is_last_repeated = 0;
+        waiting_time = 0;
+        first_start = get_waiting_time[1].start;
+    }
+
+    //remove the first element from list
+    get_waiting_time.RemoveAt(0);
+    //update id
+    id = get_waiting_time[0].process_id;
+}*/
